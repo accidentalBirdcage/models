089af953c28a5bba624d36407337178279deb178
bh90210 <ktc@protonmail.ch>
2022-04-27 01:33:28 +0200

    fix: nudge

--- generate/generate.go
+++ generate/generate.go
@@ -84,17 +84,17 @@ func (p *player) play(project *models.Project, patterns []pattern) {
 	if len(patterns) != 0 {
 		// Tempo is set by the tempo value of the first pattern to be played.
 		tempo := patterns[0].tempo
+		// /16 = 64th note per beat.
 		p.tick = time.NewTicker(time.Duration((60000/(tempo))/16) * time.Millisecond)
 		for _, pat := range patterns {
 			go func() {
 				for {
-					tempo = <-p.tempo
-					p.tick.Reset(time.Duration(60000/(tempo)/16) * time.Millisecond)
+					p.tick.Reset(time.Duration(60000/(<-p.tempo)/16) * time.Millisecond)
 				}
 			}()
 
 			if tempo != pat.tempo {
-				p.newTempo(pat.tempo)
+				p.tick.Reset(time.Duration(60000/(pat.tempo)/16) * time.Millisecond)
 			}
 
 			for i := 0; i < pat.len; i++ {
@@ -103,28 +103,28 @@ func (p *player) play(project *models.Project, patterns []pattern) {
 				}
 
 				<-p.tick.C
-				for tra, tri := range pat.tracks {
-					if tri != nil {
-						if tri.trigs[i] != nil {
-							// TODO: nudge
-							// tik := time.NewTimer(500 * time.Millisecond)
-							// <-tik.C
+				for k, track := range pat.tracks {
+					if track != nil {
+						if track.trigs[i] != nil {
+							go func(k models.Track, trig *trig) {
+								tik := time.NewTimer((time.Duration((60000/pat.tempo/16)+trig.nudge) * time.Millisecond))
+								<-tik.C
 
-							t := tri.trigs[i]
-							// Tempo change check.
-							if t.tempo != 0 {
-								p.newTempo(t.tempo)
-							}
+								// Tempo change check.
+								if trig.tempo != 0 {
+									p.newTempo(trig.tempo)
+								}
 
-							// Preset change check.
-							if t.preset != nil {
-								project.Preset(tra, t.preset)
-							}
+								// Preset change check.
+								if trig.preset != nil {
+									project.Preset(k, trig.preset)
+								}
 
-							// Noteon check.
-							if t.key != 0 && t.vel != 0 && t.dur != 0.0 {
-								project.Note(tra, t.key, t.vel, t.dur)
-							}
+								// Noteon check.
+								if trig.key != 0 && trig.vel != 0 && trig.dur != 0.0 {
+									project.Note(k, trig.key, trig.vel, trig.dur)
+								}
+							}(k, track.trigs[i])
 						}
 					}
 				}
@@ -143,7 +143,8 @@ func (p *player) newTempo(t float64) {
 
 func (g *generator) generate() []pattern {
 	var pat []pattern
-	var tempo []float64 = []float64{100.0, 50.0}
+	var tempo []float64 = []float64{70.0, 70.0}
+	var n []float64 = []float64{0.0, 300.0}
 	trigPos := []int{0, 16, 32, 48, 64, 80}
 	for j := 0; j < 2; j++ {
 		p := pattern{
@@ -157,10 +158,11 @@ func (g *generator) generate() []pattern {
 				trigs: make(map[int]*trig),
 			}
 
-			p.tracks[models.Track(i)].trigs[trigPos[i]] = &trig{
-				key: models.A4,
-				vel: 127,
-				dur: 25.0,
+			p.tracks[models.Track(i%1)].trigs[trigPos[i]] = &trig{
+				key:   models.A4,
+				vel:   127,
+				dur:   25.0,
+				nudge: n[i%2],
 			}
 		}
 
